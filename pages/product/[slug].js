e4884100e95264b911d3da210ffe806082c68850
Ian Shaw <ianshaw93@gmail.com>
2022-09-11 22:46:45 +0100

    added products you may like carousel and buy buttons onto product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
@@ -7,17 +7,62 @@ import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
   return (
     <div>
         <div className="product-detail-container">
-            <div className="image-container">
-                <img 
-                src={urlFor(image && image[0])} 
-                className="product-detail-image" 
-                />
+            <div>
+                <div className="image-container">
+                    <img 
+                    src={urlFor(image && image[0])} 
+                    className="product-detail-image" 
+                    />
+                </div>
+            </div>
+        <div className="product-detail-desc">
+            <h1>{name}</h1>
+            <div className="reviews">
+                <div>
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiOutlineStar />
+                </div>
+                <p>
+                    (20)
+                </p>
+                </div>
+                <h4>Details: </h4>
+                <p>{details}</p>
+                <p className="price">${price}</p>
+                {/* is quantity needed? */}
+                <div className="quantity">
+                    <h3>Quantity:</h3>
+                    <p className="quantity-desc">
+                    <span className="minus" ><AiOutlineMinus /></span>
+                    <span className="num">0</span>
+                    <span className="plus" o><AiOutlinePlus /></span>
+                    </p>
+                </div>
+                {/* TODO: add size selector */}
+                <div className="buttons">
+                    <button type="button" className="add-to-cart" >Add to Cart</button>
+                    <button type="button" className="buy-now" >Buy Now</button>
+                </div>
             </div>
         </div>
-        Product
+        // Product
+        <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
